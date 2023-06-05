feab2f8c90a81318cb71d26fa0a1092152b840e9
Rokuti <rockasstank@gmail.com>
2023-06-05 20:22:56 +0300

    comitinu

--- pamoku-failai/Pamoka27/src/uzduotis6iskaidytiSkaitmenis.java
+++ pamoku-failai/Pamoka27/src/uzduotis6iskaidytiSkaitmenis.java
@@ -6,16 +6,19 @@ public class uzduotis6iskaidytiSkaitmenis {
         System.out.println("Iveskite skaiciu kuri norite isskaidyti skaimenimis :");
         int num = scanner.nextInt();
         String number = String.valueOf(num);
-//        iskaidytasSkaicius(number);
-        recursion(num);
+        System.out.println(iskaidytasSkaicius(number));
+//        recursion(num);
     }
-    public static void iskaidytasSkaicius (String n){
+    public static int iskaidytasSkaicius (String n){
+        int suma = 0;
         for(int i = 0; i < n.length(); i++) {
-            int j = Character.digit(n.charAt(i), 10);
-            System.out.println("digit: " + j);
-        }
+            int j = Character.getNumericValue(n.charAt(i));
+            System.out.println(j);
+            suma += j;
+        } return suma;
     }
     public static void recursion(int number) {
+        int sum = 0;
         if(number > 0) {
             recursion(number/10);
             System.out.printf("%d  + ", (number%10));
