c2d0d40a109ceea91ca07cf86c5f0578b5ecfb5a
Rokuti <rockasstank@gmail.com>
2023-07-05 16:01:49 +0300

    suDarbuotojas

---
+++ pamoku-failai/pamoka38/sqlCode.sql
@@ -0,0 +1,22 @@
+INSERT into darbuotojas (
+    id,
+    vardas,
+    pavardė,
+    telefono_numeris,
+    pareigos,
+    bazinė_alga,
+    stažas,
+    etatas
+    ) VALUES(
+1,
+'Vardinis',
+'Pavardinis',
+ 868686868,
+ 'vadovas',
+ 2000,
+1.75,
+1),
+(2,    'Arvydas',     'Arvydauskas' ,  865554545,           'vadovas',         2000,          3,        1),
+(3,    'Vytautas',    'Vytautauskas',   868686642,         'vairuotojas',       1200,          2,        1),
+(4,    'Karolis',     'Karolinis',      868686123,         'vairuotojas',       1200,         0.5,      0.25),
+(5,    'Mantas',      'Mantauskas',     865656545,    'sandėlio operatorius',    800,          1,        1);
